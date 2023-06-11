5ed66bf79d89dd410f27bd1c9293a85908f8e445
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-06-11 15:55:24 +0300

    Made layout for header

--- my-contacts-front/src/components/Header.tsx
+++ my-contacts-front/src/components/Header.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styles from '../scss/Header.module.scss';
 
 export const Header: React.FC = () => {
+  const [activeIndex, setActive] = React.useState(0);
+
   return (
     <div className={styles.container}>
       <span className={styles.logo}>My Contacts</span>
-      <div>
-        <section>All contacts</section>
-        <section>Create contact</section>
+      <div className={styles.sections}>
+        <section className={activeIndex === 0 && styles.active} onClick={() => setActive(0)}>
+          All contacts
+        </section>
+        <section className={activeIndex === 1 && styles.active} onClick={() => setActive(1)}>
+          Create contact
+        </section>
       </div>
     </div>
   );
