d406bfbba909d89dd3ce8dce23295804d783f820
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-06-11 13:57:38 +0300

    Initial commit for backend part

---
+++ my-contacts-back/my-contacts-back/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_contacts_back.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
